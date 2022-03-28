4f01937c82fc47e820a926ca9453a4d16d5a051a
Low-profile <maxandy@naver.com>
2022-03-28 23:04:46 +0900

    New Update

--- src/components/MainNote.js
+++ src/components/MainNote.js
@@ -3,39 +3,39 @@ import './MainNote.scss';
 
 function MainNote() {
     return (
-            <div className="MainListContainer">
-                <div className="MainListBox">
-                    <h3 className="MainListName">전체메모(10)</h3>
-                </div>
-                <href to="./Note1">
-                    <div className="MainListBox">
-                        <h3 className="MainListName">라벨1(0)</h3>
-                    </div>
-                </href>
-                <href to="./Note2">
-                    <div className="MainListBox">
-                        <h3 className="MainListName">라벨2(1)</h3>
-                    </div>
-                </href>
-                <href to="./Note3">
-                    <div className="MainListBox">   
-                        <h3 className="MainListName">라벨3(2)</h3>
-                    </div>
-                </href>
-                <href to="./Note4">
-                    <div className="MainListBox">
-                        <h3 className="MainListName">라벨4(3)</h3>
-                    </div>
-                </href>
-                <href to="./Note5">
-                    <div className="MainListBox">
-                        <h3 className="MainListName">라벨5(4)</h3>
-                    </div>
-                </href>
-                <div className="MainListBox" style={{borderTop : 'solid', position:'fixed', bottom:224.5}}>
-                    <h3 className="MainListName">추가하기</h3>
-                </div>
+        <div className="MainListContainer">
+            <div className="MainListBox">
+                <h3 className="MainListName">전체메모(10)</h3>
             </div>
+            <href to="./Note1">
+                <div className="MainListBox">
+                    <h3 className="MainListName">라벨1(0)</h3>
+                </div>
+            </href>
+            <href to="./Note2">
+                <div className="MainListBox">
+                    <h3 className="MainListName">라벨2(1)</h3>
+                </div>
+            </href>
+            <href to="./Note3">
+                <div className="MainListBox">
+                    <h3 className="MainListName">라벨3(2)</h3>
+                </div>
+            </href>
+            <href to="./Note4">
+                <div className="MainListBox">
+                    <h3 className="MainListName">라벨4(3)</h3>
+                </div>
+            </href>
+            <href to="./Note5">
+                <div className="MainListBox">
+                    <h3 className="MainListName">라벨5(4)</h3>
+                </div>
+            </href>
+            <div className="MainListBox" style={{ borderTop: 'solid', position: 'fixed', bottom: 105.5 }}>
+                <h3 className="MainListName">추가하기</h3>
+            </div>
+        </div>
     );
 };
 
--- src/components/SubNote.js
+++ src/components/SubNote.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './SubNote.scss';
 
-function SubNote(){
-    return(
-            <div className="SubListContainer">
+function SubNote() {
+    return (
+        <div className="SubListContainer">
             <div className="SubListBox">
                 <h3 className="SubListName">라벨3</h3>
                 <button style={{ height: 20, marginTop: 23, marginLeft: 10 }}>수정</button>
@@ -13,12 +13,12 @@ function SubNote(){
             <div className="SubListBox">
                 <h3 className="SubListName" style={{ marginTop: 10, marginLeft: 15 }}>Test1</h3>
                 <p className="SubListContent" style={{ marginTop: 40, marginLeft: -70 }}>미리보기 내용입니다.</p>
-                <p className="SubListDate" style={{ marginTop: 15, marginLeft: 20 }}>2022.03.08</p>
+                <p className="SubListDate" style={{ marginTop: 15, marginLeft: 20 }}>2022.03.10</p>
             </div>
             <div className="SubListBox">
                 <h3 className="SubListName" style={{ marginTop: 10, marginLeft: 15 }}>Test2</h3>
                 <p className="SubListContent" style={{ marginTop: 40, marginLeft: -70 }}>미리보기 내용입니다.</p>
-                <p className="SubListDate" style={{ marginTop: 15, marginLeft: 20 }}>2022.03.07</p>
+                <p className="SubListDate" style={{ marginTop: 15, marginLeft: 20 }}>2022.03.09</p>
             </div>
         </div>
 
